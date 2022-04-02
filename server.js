9b958a01f118eea1157c62573420066249b07bf0
DevshanL <it20109462@my.sliit.lk>
2022-04-02 15:57:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+//Import expres package & mongoose package by require
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app =express();
+
+const PORT = 8000;
+const DB_URL = 'mongodb+srv://adikt:adikt123@adiktdb.baouy.mongodb.net/AdiktExportsDB?retryWrites=true&w=majority';
+
+
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log('Mongodb Successfully Connected');
+  })
+  .catch((err) => console.log('mongodb connection Failed',err));
+  
+
+
+app.listen(PORT, () =>{
+    console.log(`App is running on ${PORT}`);
+});
