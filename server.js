06812feb25875151bac3445d3c528227f9af6b84
DevshanL <it20109462@my.sliit.lk>
2022-04-04 14:01:25 +0530

    modify server.js

--- server.js
+++ server.js
@@ -2,8 +2,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+//coming to server json format, so convert to js format
+const bodyparser = require("body-parser");
+
 
 const app =express();
+
+const inventoryRoutes = require("./routes/inventories");
+
+//middleware
+app.use(bodyparser.json());
+
+
+app.use(inventoryRoutes);
 
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://adikt:adikt123@adiktdb.baouy.mongodb.net/AdiktExportsDB?retryWrites=true&w=majority';
@@ -20,3 +31,6 @@ mongoose.connect(DB_URL)
 app.listen(PORT, () =>{
     console.log(`App is running on ${PORT}`);
 });
+
+
+module.exports = mongoose;
