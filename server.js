aef021644eedd1cf159602ab03cf306d7d9214f5
yomaavitharana <it20016784@my.sliit.lk>
2022-05-08 02:37:06 +0530

    buyer register routes added

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ const productionRoutes = require("./routes/productions");
 
 //exportdetails
 const exportDetails = require("./routes/exportDetails");
+
+const buyerRegister = require("./routes/buyerRegister");
+
+//const buyerRegister = require('./models/buyerRegister');
 //middleware
 app.use(bodyparser.json());
 app.use(cors());
@@ -33,8 +37,9 @@ app.use(inventoryRoutes);//Inventory
 app.use(productionRoutes);//Production
 
 app.use(requestRoutes);//Customer requests
-app.use(qcRoutes); //Quality Check
+// app.use(qcRoutes); //Quality Check
 app.use(exportDetails);//Export Details
+app.use(buyerRegister);//Buyer Register
 
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://adikt:adikt123@adiktdb.baouy.mongodb.net/AdiktExportsDB?retryWrites=true&w=majority';
